5ef82207f4b602d535e0566ef9da338ffac2d85e
Master Admin <master@newkorea.org>
2026-04-27 15:39:47 +0900

    Cleanup: Optimized for Core Functions Only (Surveys, Diagnostics, Ministry Test, Workshop, Weekly Report).

--- master_data.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    dioceses: ["Seoul", "Incheon", "Gyeonggi_North", "Gyeonggi_South", "Gangwon", "Chungbuk", "Chungnam", "Jeonbuk", "Jeonnam", "Gyeongbuk", "Gyeongnam", "Jeju", "Daegu", "Busan"],
-    departments: ["GeneralAffairs", "Finance", "Mission", "Education", "External", "Culture", "Youth"],
-    presentationOrder: ["GeneralAffairs", "Seoul", "Incheon", "Busan"]
-};
